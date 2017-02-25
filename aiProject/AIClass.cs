45f778f52692d920d6dd10db26246411b18e99c2
yurippe <yurippenet@gmail.com>
2017-02-25 22:58:26 +0100

    initial commit
    Should contain everything to get started, clean install w/old AI as
    oponent

---
+++ aiProject/AIClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Torque3D;
+using Torque3D.Engine.Util.Enums;
+using Torque3D.Util;
+
+namespace GameAI
+{
+   public class AIClass
+   {
+      [ConsoleFunction]
+      public static PlayerAction MyThinkMethod(FeatureVector vector)
+      {
+         // Implement your AI logic here, compile and then run Game.exe
+         return PlayerAction.MoveForward;
+      }
+   }
+}

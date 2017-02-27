02490f85efb5d20896548d88eecaa8d0c31f0874
yurippe <yurippenet@gmail.com>
2017-02-27 02:06:00 +0100

    Added reset logic
    Also started on map management

--- aiProject/Brain.cs
+++ aiProject/Brain.cs
@@ -10,23 +10,41 @@ namespace RealAI {
     public class Brain
     {
 
-        private List<EventListener> eventListeners = new List<EventListener>();
-        private State currentState = new InitialState();
+        private List<EventListener> eventListeners;
+        private State currentState;
+        private MapManager mapManager;
 
-        private bool _hasPrepareMove = false;
-        private int _lastPrepareTick = -1;
+        private bool _hasPrepareMove;
+        private int _lastPrepareTick;
 
         private FeatureVector currentVector;
         private FeatureVector previousVector;
 
         public Brain()
         {
+            Reset();
+        }
+
+        //Yeh, the game doesn't do this for us xD
+        private void Reset()
+        {
+            eventListeners = new List<EventListener>();
+            currentState = new InitialState(); //Reset our states
             registerEvents();
+            mapManager = new MapManager(); //Reset the map
+            //Reset fields
+            _hasPrepareMove = false;
+            _lastPrepareTick = -1;
+            currentVector = null;
+            previousVector = null;
+
         }
 
         public PlayerAction tick(FeatureVector vector)
         {
+            if(vector.TickCount == 3200) { Reset(); return PlayerAction.Prepare; } //This is the reset condition, guess we also need to return a non-null value
             currentVector = vector;
+            mapManager.tick(vector, this);
             triggerEvents();
             //States return the next state; this is an infinite stream of states;
             PlayerAction action = PlayerAction.Prepare; //This is ALWAYS better than doing nothing
@@ -61,6 +79,8 @@ namespace RealAI {
         //return the first vector as the previous if it is the first tick, this works for most cases
         public FeatureVector getPreviousVector() { return currentVector.TickCount == 1 ? currentVector : previousVector; } 
 
+        public MapManager getMapManager() { return mapManager; }
+
         //The prepare move is defined as the first of the 2 moves
         public bool isPreparedMove() { return _hasPrepareMove && getCurrentVector().TickCount != 1 && getCurrentVector().TickCount != getPreviousVector().TickCount; }
         public bool hasCachedPrepareMove() { return _hasPrepareMove; }
@@ -74,7 +94,6 @@ namespace RealAI {
         }
 
         #endregion
-
 
 
         private void triggerEvents()
---
+++ aiProject/MapManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torque3D.Util;
+
+namespace RealAI
+{
+    public class MapManager
+    {
+        float playerX = 0.0f;
+        float playerY = 0.0f;
+
+
+        public void tick(FeatureVector vector, Brain brain)
+        {
+
+        }
+
+        public string renderMap()
+        {
+            return "HAH, you wish!";
+        }
+
+    }
+}
